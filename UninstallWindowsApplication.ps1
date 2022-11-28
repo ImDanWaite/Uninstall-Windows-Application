cb59435547d20f164e0606b84519bea0987007f2
ImDanWaite <103328674+ImDanWaite@users.noreply.github.com>
2022-11-28 14:15:42 +0000

    Update UninstallWindowsApplication.ps1

--- UninstallWindowsApplication.ps1
+++ UninstallWindowsApplication.ps1
@@ -1,2 +1,5 @@
-﻿$Snow = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Snow Inventory Agent"}
+#'$Snow' can be anything related to app - e.g. $iTunes
+#{$_.Name -eq "(name of app in programs and features)"} - e.g. #{$_.Name -eq "iTunes"}
+
+$Snow = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Snow Inventory Agent"}
 $Snow.uninstall()
